6896febcd46d4be028399864ca68cca1d6fbda1e
Elliott Mejia <mejia.elliott@gmail.com>
2023-11-20 14:11:39 -0800

    Fixed react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
--- src/components/react/converted/Accordion/Accordion.tsx
+++ src/components/react/converted/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AccordionItem from "./AccordionItem.jsx";
 
 interface Props {
@@ -11,40 +11,6 @@ interface Props {
 }
 
 export default function Accordion(props: Props) {
-    const [active, setActive] = useState(null);
-    // END: ed8c6549bwf9
-
-    const handleToggle = (index) => {
-        if (active === index) {
-            setActive(null);
-        } else {
-            setActive(index);
-        }
-    }
-
-    return (
-        <>
-            <div className="container-fluid mt-5 mb-5 w-4/5 mx-auto">
-                <div className="row justify-content-center">
-                    <div className="col-md-8 mt-2">
-                        <div className="card">
-                            <div className="card-body">
-                                <h4 className="form-heading mb-4 text-primary text-center text-4xl mt-3">{props.title}</h4>
-                                {props.data.map((item, index) => {
-                                    return (
-                                        <AccordionItem key={index} active={active} handleToggle={handleToggle}
-                                            item={item} />
-                                    )
-                                })
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
     const [active, setActive] = useState(null);
 
     const handleToggle = (index) => {
