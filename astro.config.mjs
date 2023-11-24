d185c8b8f4ac652e85882ad790779f7fd51afd32
Elliott Mejia <mejia.elliott@gmail.com>
2023-11-23 23:11:38 -0800

    MAIN-T-51 VERCEL FIXED

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
+
+let adapter = vercel();
+
+if (process.argv[3] === "--node" || process.argv[4] === "--node") {
+  adapter = node({ mode: "standalone" });
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://myhonestpower.com",
-  experimental: {
-    viewTransitions: true,
-  },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    react(),
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), react()],
   output: "server",
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
+  adapter: adapter,
 });
