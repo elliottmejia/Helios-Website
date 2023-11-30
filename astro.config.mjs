137d6db5710ff782621307ea35f52c4f10b61e38
Elliott Mejia <mejia.elliott@gmail.com>
2023-11-29 18:22:34 -0800

    MAIN-T-91 partytown/GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,22 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), react()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel(),
 });
